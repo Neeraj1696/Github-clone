8078df40c73040c1276aa59c05084b7db193cb32
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-08-17 18:37:15 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useFetch } from "./useFetch";
 
 function App() {
+  const { loading, data } = useFetch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="section_title">
+        <h1>{loading ? "Loading..." : "Profile"}</h1>
+      </div>
+    </main>
   );
 }
 
